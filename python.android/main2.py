7795de71466d848c012d861ffeffba6c9e145db1
francesco.cerone <francesco.cerone.drum@gmail.com>
2016-11-26 18:25:43 +0100

    added gif button

--- python.android/main2.py
+++ python.android/main2.py
@@ -1,50 +1,55 @@
+'''
+Audio example
+=============
+
+This example plays sounds of different formats. You should see a grid of
+buttons labelled with filenames. Clicking on the buttons will play, or
+restart, each sound. Not all sound formats will play on all platforms.
+
+All the sounds are from the http://woolyss.com/chipmusic-samples.php
+"THE FREESOUND PROJECT", Under Creative Commons Sampling Plus 1.0 License.
+
+'''
+
 import kivy
 kivy.require('1.0.8')
 
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty, ObjectProperty, NumericProperty
+from kivy.properties import StringProperty
 from glob import glob
 from os.path import dirname, join, basename
 from kivy.lib.osc import oscAPI
 import os
 
 
-class FlashButton(Button):
-
+class ImageButton(Button):
     filename = StringProperty(None)
-    sound = ObjectProperty(None, allownone=True)
-    volume = NumericProperty(1.0)
-
     def on_press(self):
         print os.path.basename(self.filename)
         oscAPI.init()
         oscAPI.sendMsg('/toSetImage', dataArray=[os.path.basename(self.filename)], ipAddr='localhost', port=57110)
-
-   
-
+        
 
 class AudioBackground(BoxLayout):
     pass
 
 
-class AudioApp(App):
+class ControllerApp(App):
 
     def build(self):
-
         root = AudioBackground(spacing=5)
         
         path = os.path.dirname(__file__) + "/resources/"
         for fn in glob(join(dirname(path ), '*.*')):
-            btn = FlashButton(
+            btn = ImageButton(
                 text=basename(fn[:-4]).replace('_', ' '), filename=fn,
                 size_hint=(None, None), halign='center',
                 size=(128, 128), text_size=(118, None))
             root.ids.sl.add_widget(btn)
 
         return root
-
 
     def release_audio(self):
         for audiobutton in self.root.ids.sl.children:
@@ -55,4 +60,4 @@ class AudioApp(App):
             audiobutton.set_volume(value)
 
 if __name__ == '__main__':
-    AudioApp().run()
+    ControllerApp().run()

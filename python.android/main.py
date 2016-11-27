5ee71dca88892f8a3c608f21958afb7d15cd3682
francesco.cerone <francesco.cerone.drum@gmail.com>
2016-11-27 20:43:29 +0100

    refined slider

--- python.android/main.py
+++ python.android/main.py
@@ -9,20 +9,27 @@ from glob import glob
 from os.path import dirname, join, basename
 from kivy.lib.osc import oscAPI
 import os
+from kivy.uix.slider import Slider
 
 
-class FlashButton(Button):
-
+class AudioButton(Button):
     filename = StringProperty(None)
     sound = ObjectProperty(None, allownone=True)
     volume = NumericProperty(1.0)
-
     def on_press(self):
         print os.path.basename(self.filename)
         oscAPI.init()
         oscAPI.sendMsg('/toSetImage', dataArray=[os.path.basename(self.filename)], ipAddr='localhost', port=57110)
 
-   
+
+class FlashButton(Button):
+    def on_press(self):
+        print "Flash Button pressed"
+        
+class ColorSlider(Slider):
+    def on_touch_up(self, touch):
+        print "Change color value"
+      
 
 
 class AudioBackground(BoxLayout):
@@ -37,22 +44,33 @@ class AudioApp(App):
         
         path = os.path.dirname(__file__) + "/resources/"
         for fn in glob(join(dirname(path ), '*.*')):
-            btn = FlashButton(
+            btn = AudioButton(
                 text=basename(fn[:-4]).replace('_', ' '), filename=fn,
                 size_hint=(None, None), halign='center',
                 size=(128, 128), text_size=(118, None))
             root.ids.sl.add_widget(btn)
 
+        flashBt = FlashButton(
+                text="Flash On Off",
+                size_hint=(None, None), halign='center',
+                size=(128, 128), text_size=(118, None))
+        
+        root.ids.sl.add_widget(flashBt)
+        
+        
+        s = ColorSlider(min=-100, max=100, value=25)
+        root.ids.sl.add_widget(s)
         return root
 
 
-    def release_audio(self):
-        for audiobutton in self.root.ids.sl.children:
-            audiobutton.release_audio()
+    #def release_audio(self):
+     #   for audiobutton in self.root.ids.sl.children:
+      #      audiobutton.release_audio()
 
     def set_volume(self, value):
-        for audiobutton in self.root.ids.sl.children:
-            audiobutton.set_volume(value)
+        print "Set Volume"
+     #   for audiobutton in self.root.ids.sl.children:
+      #      audiobutton.set_volume(value)
 
 if __name__ == '__main__':
     AudioApp().run()

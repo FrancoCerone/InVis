551c6b7cd0f8befe5d776fb5ce24101f37ba36bd
francesco.cerone <francesco.cerone.drum@gmail.com>
2017-04-15 13:26:33 +0200

    aggiunta modalita lineare

--- python.dialog/ScreenResolution.py
+++ python.dialog/ScreenResolution.py
@@ -3,11 +3,17 @@ Created on 14 mar 2017
 
 @author: francescocerone
 '''
+
+
 class ScreenResolution(object):
     
     def __init__(self):
-        self.width = 1366
-        self.height = 768 
+        #mac
+        self.width = 2560
+        self.height = 1600
+        #windos
+        #self.width = 1366
+        #self.height = 768 
     
     
     def get_width(self):
--- python.dialog/pong.py
+++ python.dialog/pong.py
@@ -11,12 +11,14 @@ from kivy.clock import Clock
 from kivy.lang import Builder
 from ScreenResolution import ScreenResolution
 from kivy.graphics import Color, Rectangle
-
+from kivy import animation
+from math import log10
+from random import randint
 
 Builder.load_string("""
 <PongBall>:
     size: 100, 100
-    pos:   1366, -50
+    pos:   0, 0
     canvas:
         Ellipse:
             pos: self.pos
@@ -25,21 +27,51 @@ Builder.load_string("""
     
 """)
 
+
+screenResolution = ScreenResolution()
+out_file = open("test.txt","w")
+
 class ImageDispatcher():
     image = ObjectProperty
+    modality = ObjectProperty
+    lastHeightGiven = NumericProperty
     def set_image(self, image):
         self.image = image;
     def get_image(self):
-        return 'resources/pngs/'+self.image+'.png';
+        return 'resources/animations/'+self.image+'.png';
     
-screenResolution = ScreenResolution()
+    def set_modality(self, modality):
+        self.modality = modality;
+    def get_modality(self):
+        return self.modality;
+    
+    def get_ball_hegth(self):
+        heigth = randint(0, screenResolution.get_height()) 
+        module = heigth % 100
+        heigth = heigth - module
+        if( heigth == self.lastHeightGiven):
+            heigth = heigth - 100
+        self.lastHeightGiven = heigth
+        print "calculated height: ", heigth
+        return heigth
+        
+    
+
 
 imageDispatcher = ImageDispatcher()
 
 class PongBall(Widget):
-    def move(self, x,y ):
-        x= -x
-        self.pos = Vector(x,y) + self.pos
+    modality = ObjectProperty
+    def set_height(self, height):
+        print "setted height", height
+        self.pos[1] = height
+        
+    def move(self, x ):
+        x= self.pos[0] +x
+        y= self.pos[1]
+  
+        out_file.close()
+        self.pos = Vector(x,y) 
         
     def get_image(self):
         print "da image disp", imageDispatcher.get_image()
@@ -47,6 +79,11 @@ class PongBall(Widget):
     
     def set_image(self, image):
         print image
+    
+    def set_modality(self, modality):
+        self.modality = modality;
+    def get_modality(self):
+        return self.modality;
 
 
     
@@ -58,17 +95,25 @@ class PongGame(Widget):
         
     def update(self, dt):
         for amimation in self.balls:
-            amimation.move(3, 3)
-            if(amimation.pos.__getitem__(0) > 0):
+            amimation.move(3)
+            #print "image with modality: ", amimation.get_modality()
+            if(amimation.pos.__getitem__(0) < screenResolution.get_width()):
                 continue
             else:
                 self.balls.remove(amimation)
+                out_file.close()
                 if (self.balls.__len__() == 0):
                     return    
     
-    def add_animation(self, image):
+    def add_animation(self, image, modality):
+        print "Start animation with image", image, " on modality", modality
         imageDispatcher.set_image(image)
+        imageDispatcher.set_modality(modality)
+        heightToStart = imageDispatcher.get_ball_hegth()
+        
         newAnimation = PongBall()
+        newAnimation.set_height(heightToStart)
+        newAnimation.set_modality(modality)
         self.balls.append(newAnimation)
         self.add_widget(newAnimation)
         print "Dimensione delle animazioni", self.balls.__len__()

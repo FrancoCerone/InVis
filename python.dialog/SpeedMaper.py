16c3a3ff96562d99703de4934b0b1f46b2aba896
francesco.cerone <francesco.cerone.drum@gmail.com>
2017-08-05 09:48:14 +0200

    added resources

--- python.dialog/SpeedMaper.py
+++ python.dialog/SpeedMaper.py
@@ -8,8 +8,8 @@ Created on 14 mar 2017
 class SpeedMapper(object):
     
     speedMap = {
+        "0i" :  "1",
         "1g" :  "0.3",
         "1n" : "0.3",
-        "1o  " : "0.3",
-        
+        "1o" : "0.3",
         }

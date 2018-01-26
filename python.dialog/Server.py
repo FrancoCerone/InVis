7f69f5754090edfbe640a04e4d92cf3488a0fe4a
francesco.cerone <francesco.cerone.drum@gmail.com>
2018-01-26 13:20:34 +0100

    initila contribution for Camera

---
+++ python.dialog/Server.py
@@ -0,0 +1,37 @@
+'''
+Created on 26 gen 2018
+
+@author: francescocerone
+'''
+### Server
+import socket
+from threading import *
+import socket
+import pygame
+import pygame.camera
+from pygame.locals import *
+ 
+ 
+def server():
+    try:
+        #print "\nServer started at " + str(socket.gethostbyname(socket.gethostname())) + " at port " + str(90)  
+        port = 8085
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serversocket.bind(("",port))
+        serversocket.listen(10)
+        pygame.camera.init()
+        cam = pygame.camera.Camera(0,(640,480),"RGB")
+        cam.start()
+        img = pygame.Surface((640,480))
+        while True:
+            connection, address = serversocket.accept()
+            print "GOT_CONNECTION"
+            cam.get_image(img)
+            data = pygame.image.tostring(img,"RGB")
+            connection.sendall(data)
+            connection.close()
+    except Exception,e:
+        print str(e)
+        pass
+ 
+server()

e927da99182c4edc02905565f9d0e0e7b3312cb4
franco <franco@franco-Vostro-5568>
2018-02-10 15:07:48 +0100

    added message to manage un mask

--- python.raspberry/MuskCommander.py
+++ python.raspberry/MuskCommander.py
@@ -39,17 +39,13 @@ class Network():
 
 
 class RaspBerryApp(App):
-    _color = Color(1, 1, 1)
-    _isFlashRunning = False
-    _isRersistable = True
-    _modality=0
-    a=True
-    couter=0
     
     def build(self):
         oscAPI.init()
         oscid = oscAPI.listen(ipAddr=Network.ip, port=57110) # per elektroWave WiFi: 192.168.0.12
         oscAPI.bind(oscid, self.setStatus, '/toSetMusk')
+        oscAPI.bind(oscid, self.setStatusMask1, '/toSetMusk1')
+        oscAPI.bind(oscid, self.setStatusMask2, '/toSetMusk2')
         Clock.schedule_interval(lambda *x: oscAPI.readQueue(oscid), 0)
         self._parent = Widget()
         return self._parent
@@ -59,6 +55,14 @@ class RaspBerryApp(App):
         print 'arrivato il messaggio' , message[2]
         GPIO.output(8,message[2])
         GPIO.output(10,message[2])
-
+        
+    def setStatusMask1(self, message, *args):
+        print 'arrivato il messaggio' , message[2]
+        GPIO.output(8,message[2])
+        
+    def setStatusMask2(self, message, *args):
+        print 'arrivato il messaggio' , message[2]
+        GPIO.output(10,message[2])
+        
 if __name__ == '__main__':
     RaspBerryApp().run()
